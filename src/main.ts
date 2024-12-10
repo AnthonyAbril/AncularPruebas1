413dc0a72265e328ac7d93a5c784da3cb90c701b
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-12-10 20:09:45 +0100

    desestructuracion funciones

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import './pruebas - ts/01-tipos-basicos'
 import './pruebas - ts/02-interface'
 import './pruebas - ts/03-funciones'
 import './pruebas - ts/04-desestructuracion'
+import './pruebas - ts/05-desestructuracion-funcion-inicial'
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
